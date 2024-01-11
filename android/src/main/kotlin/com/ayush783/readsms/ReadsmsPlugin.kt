e71ff1ec79d9117a872ad0422e16a3698fc55970
Omar Abdallah <71341894+omarabdallah96@users.noreply.github.com>
2024-01-11 14:52:58 +0200

    fix  on detach

--- android/src/main/kotlin/com/ayush783/readsms/ReadsmsPlugin.kt
+++ android/src/main/kotlin/com/ayush783/readsms/ReadsmsPlugin.kt
@@ -67,14 +67,14 @@ class ReadsmsPlugin: FlutterPlugin, EventChannel.StreamHandler,BroadcastReceiver
   }
 
   override fun onDetachedFromActivityForConfigChanges() {
-    TODO("Not yet implemented")
+    // TODO("Not yet implemented")
   }
 
   override fun onReattachedToActivityForConfigChanges(p0: ActivityPluginBinding) {
-    TODO("Not yet implemented")
+    // TODO("Not yet implemented")
   }
 
   override fun onDetachedFromActivity() {
-    TODO("Not yet implemented")
+    // TODO("Not yet implemented")
   }
 }
