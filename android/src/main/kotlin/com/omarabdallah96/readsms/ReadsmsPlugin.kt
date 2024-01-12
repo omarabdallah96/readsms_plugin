ccd6f3b4fc8a3f2e3b98df5c7e3b16b9a6e564fc
Omar Abdallah <71341894+omarabdallah96@users.noreply.github.com>
2024-01-12 13:49:07 +0200

    s

--- android/src/main/kotlin/com/omarabdallah96/readsms/ReadsmsPlugin.kt
+++ android/src/main/kotlin/com/omarabdallah96/readsms/ReadsmsPlugin.kt
@@ -1,98 +1,95 @@
 package com.omarabdallah96.readsms
 
 import android.app.Activity
-import androidx.annotation.NonNull
-import android.telephony.SmsMessage
-import android.os.Bundle
-
-import io.flutter.embedding.engine.plugins.FlutterPlugin
-import io.flutter.plugin.common.EventChannel
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.content.IntentFilter
 import android.os.Build
-import android.provider.Telephony
+import android.os.Bundle
+import android.telephony.SmsMessage
 import android.util.Log
+import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.plugin.common.EventChannel
 import io.flutter.embedding.engine.plugins.activity.ActivityAware
 import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
 
-class ReadsmsPlugin: FlutterPlugin, EventChannel.StreamHandler,BroadcastReceiver(), ActivityAware {
-  private var channel : EventChannel? = null
+class ReadsmsPlugin : FlutterPlugin, EventChannel.StreamHandler, BroadcastReceiver(), ActivityAware {
+    private var channel: EventChannel? = null
+    private var eventSink: EventChannel.EventSink? = null
+    private lateinit var context: Context
+    private lateinit var activity: Activity
 
-  private var eventSink: EventChannel.EventSink? = null
-  /**
-   * context object to get the current context and register
-   * the broadcast receiver
-   */
-  private lateinit var context: Context
-  private lateinit var activity: Activity
-
-
-  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-    context = flutterPluginBinding.applicationContext
-    context.registerReceiver(this,IntentFilter("android.provider.Telephony.SMS_RECEIVED"))
-    channel = EventChannel(flutterPluginBinding.binaryMessenger,"readsms")
-    channel!!.setStreamHandler(this)
-  }
-
-  override fun onListen(arguments: Any?, events: EventChannel.EventSink?) {
-    eventSink = events
-  }
-
-  override fun onCancel(arguments: Any?) {
-    eventSink = null
-  }
-
-  override fun onReceive(p0: Context?, p1: Intent?) {
-    /**
-     * Get the messages through the broadcast receiver
-     * using the Telephony.Sms.Intent
-     */
-    if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT){
-      for (sms in Telephony.Sms.Intents.getMessagesFromIntent(p1)) {
-        // Log.d("msg sender", sms.originatingAddress.toString())
-        // Log.d("msg time",sms.timestampMillis.toString())
-                val bundle: Bundle? = intent.extras
-
-                if (bundle != null) {
-        val serviceCenterAddress = getServiceCenterAddress(sms.pdu)
-                }
-                else {
-                  val serviceCenterAddress = "N/A"
-                }
-
-
-
-        var data = listOf(sms.displayMessageBody,sms.originatingAddress.toString(),sms.timestampMillis.toString(),sms.serviceCenterAddress.toString())
-        eventSink?.success(data)
-      }
+    override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+        context = flutterPluginBinding.applicationContext
+        context.registerReceiver(this, IntentFilter("android.provider.Telephony.SMS_RECEIVED"))
+        channel = EventChannel(flutterPluginBinding.binaryMessenger, "readsms")
+        channel?.setStreamHandler(this)
     }
-  }
 
-  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
-    channel = null
-    eventSink = null
-  }
+    override fun onListen(arguments: Any?, events: EventChannel.EventSink?) {
+        eventSink = events
+    }
 
-  override fun onAttachedToActivity(p0: ActivityPluginBinding) {
-    activity = p0.activity
-  }
+    override fun onCancel(arguments: Any?) {
+        eventSink = null
+    }
 
-  override fun onDetachedFromActivityForConfigChanges() {
-    // TODO("Not yet implemented")
-  }
+    override fun onReceive(context: Context?, intent: Intent?) {
+        if (intent != null) {
+            val bundle: Bundle? = intent.extras
 
-  override fun onReattachedToActivityForConfigChanges(p0: ActivityPluginBinding) {
-    // TODO("Not yet implemented")
-  }
+            if (bundle != null) {
+                for (sms in Telephony.Sms.Intents.getMessagesFromIntent(intent)) {
+                    val serviceCenterAddress = getServiceCenterAddress(sms.pdu)
 
-  override fun onDetachedFromActivity() {
-    // TODO("Not yet implemented")
-  }
+                    var data = listOf(
+                        sms.displayMessageBody,
+                        sms.originatingAddress.toString(),
+                        sms.timestampMillis.toString(),
+                        serviceCenterAddress
+                    )
+                    eventSink?.success(data)
+                }
+            }
+            else {
+                for (sms in Telephony.Sms.Intents.getMessagesFromIntent(intent)) {
+                    val serviceCenterAddress = 'N/A'
 
-  private fun getServiceCenterAddress(pdu: ByteArray?): String {
-    val smsMessage = SmsMessage.createFromPdu(pdu)
-    return smsMessage.serviceCenterAddress ?: "N/A"
-  }
+                    var data = listOf(
+                        sms.displayMessageBody,
+                        sms.originatingAddress.toString(),
+                        sms.timestampMillis.toString(),
+                        serviceCenterAddress
+                    )
+                    eventSink?.success(data)
+                }
+            }
+        }
+    }
+
+    override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+        channel = null
+        eventSink = null
+    }
+
+    override fun onAttachedToActivity(p0: ActivityPluginBinding) {
+        activity = p0.activity
+    }
+
+    override fun onDetachedFromActivityForConfigChanges() {
+        // Handle as needed
+    }
+
+    override fun onReattachedToActivityForConfigChanges(p0: ActivityPluginBinding) {
+        // Handle as needed
+    }
+
+    override fun onDetachedFromActivity() {
+        // Handle as needed
+    }
+
+    private fun getServiceCenterAddress(pdu: ByteArray): String {
+        val smsMessage = SmsMessage.createFromPdu(pdu)
+        return smsMessage.serviceCenterAddress ?: "N/A"
+    }
 }
